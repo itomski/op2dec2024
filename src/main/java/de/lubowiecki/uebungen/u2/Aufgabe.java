943de17abe83b01df4bc9a039f89425fd51a4ec4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-12-10 13:04:17 +0100

    Aufgaben 10.12.2024

---
+++ src/main/java/de/lubowiecki/uebungen/u2/Aufgabe.java
@@ -0,0 +1,19 @@
+package de.lubowiecki.uebungen.u2;
+
+public class Aufgabe {
+
+    // Aufgabe 1
+    // Schreibe eine Utility-Klasse für einen Würfelbecher
+    // Er soll zwei Methoden enthalten:
+    // eine für einen einzigen Würfelwurf mit einem 6-Seitigen Würfel
+    // eine für einen Würfelwurf mit mehreren 6-Seitigen Würfeln. Anzahl wird als Parameter vorgegeben
+    // Beide Methoden sollen die Werte zurückgeben und nicht selbst auf der Konsole ausgeben
+
+    // Schreibe eine Taschenrechner-Klasse mit den 4 Grundrechenarten für double Werte
+    // Die Methoden sollen bei der Ausführung die Aktion in ein Protokoll schreiben
+    // z.B. bei add(10, 22) wird "10 + 22 = 32" ins Protokoll geschrieben.
+    // Der Taschenrechner soll als Singleton umgesetzt werden, so dass man sich jederzeit alle
+    // vergangenen Methoden-Aufrufe anschauen kann.
+    // mit der Methode clear() kann das Protokoll geleert werden
+
+}
