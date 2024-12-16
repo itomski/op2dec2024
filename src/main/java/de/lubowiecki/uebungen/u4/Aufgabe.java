c58f99181ea9f0497444eb8241b380f26320b720
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-12-16 13:18:57 +0100

    Aufgabe: 14.12.24

---
+++ src/main/java/de/lubowiecki/uebungen/u4/Aufgabe.java
@@ -0,0 +1,8 @@
+package de.lubowiecki.uebungen.u4;
+
+public class Aufgabe {
+
+    // Entwerfe eine Klasse Fahrzeug mit den Eigenschaften id, marke, modell, baujahr, farbe und kennzeichen
+    // Schreibe dafür eine Mapper-Klasse und teste sie
+
+}
