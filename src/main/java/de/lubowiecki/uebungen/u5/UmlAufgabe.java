f9c406db16ee4acbb1299cc0ad72c5e7e5369ef8
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-12-18 13:09:27 +0100

    Keynotes

---
+++ src/main/java/de/lubowiecki/uebungen/u5/UmlAufgabe.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.uebungen.u5;
+
+public class UmlAufgabe {
+
+    /*
+    Schreibe eine Liste mit Funktionalitäten/Anforderungen für ein FitnessStudio
+    Versuche dabei die verschiedenen angebotenen Dienste und damit verbundenen Anforderungen zu erfassen
+    Sowohl für einen Besucher als auch einen Trainer.
+    Versuche dies alls in einen Anwendungsfalldiagramm zu integrieren
+    */
+
+
+}
