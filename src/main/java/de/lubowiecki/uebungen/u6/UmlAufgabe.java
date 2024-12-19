cfb981032da01a924b6a297478fd521a3fb68d84
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-12-19 13:50:27 +0100

    Aufgabe: 19.12.2024

---
+++ src/main/java/de/lubowiecki/uebungen/u6/UmlAufgabe.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.uebungen.u6;
+
+public class UmlAufgabe {
+
+    /*
+    Modelliere ein UML Klassendiagramm, das folgenden Sachverhalt beschreibt:
+        Jede Person hat einen Namen, eine Telefonnummer und eine E-Mail Adresse.
+        Jede Wohnadresse wird von maximal einer Person genutzt.
+        Wohnadressen bestehen aus einer Straße, einer Stadt, einer Postleitzahl und einem Land.
+        Wohnadressen können bestäatigt werden und als Beschriftung gedruckt werden.
+     */
+
+}
