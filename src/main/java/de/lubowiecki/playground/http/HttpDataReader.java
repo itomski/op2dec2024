1e86d2d36d57c351ee03f2fe0281c11795d9a83f
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-12-23 09:08:43 +0100

    Http Anfragen

---
+++ src/main/java/de/lubowiecki/playground/http/HttpDataReader.java
@@ -0,0 +1,52 @@
+package de.lubowiecki.playground.http;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class HttpDataReader {
+
+    public static void main(String[] args) {
+
+        final String URL = "http://localhost:8080/api/v1/fahrzeuge";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(URL))
+                                .build();
+
+        try {
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(HttpDataReader::parseJson)
+                    .join();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String parseJson(String json) {
+
+        System.out.println(json);
+
+        /*
+        final ObjectMapper mapper = new ObjectMapper();
+        CollectionType type = mapper.getTypeFactory().constructCollectionType(List.class, Post.class);
+        try {
+            List<Post> posts = mapper.readValue(json, type);
+            System.out.println(posts);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        return null;
+    }
+}
---
+++ src/main/java/de/lubowiecki/playground/http/Post.java
@@ -0,0 +1,40 @@
+package de.lubowiecki.playground.http;
+
+public class Post {
+
+    private int id;
+    private String title;
+
+    public Post() {
+    }
+
+    public Post(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Post{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
